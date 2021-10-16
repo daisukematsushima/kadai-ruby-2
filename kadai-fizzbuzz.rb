bf86f461adb609c0432ac76a9ec085aa4665a228
daisuke.matsushima <haru56utauhito@gmail.com>
2021-10-16 13:29:56 +0000

    add kadai-fizzbuzz.rb

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,16 @@
+def fizzbuzz (num)
+        if num % 15 == 0
+            "FizzBuzz"
+        elsif num % 3 == 0
+            "Fizz"
+        elsif num % 5 == 0
+            "buzz"
+        else
+            num
+        end
+end
+
+num_max = 100
+(1..num_max).each do |num|
+    puts fizzbuzz(num)
+end
